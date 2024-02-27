a3231332217a68e8270dc87915a9296aea3ef7c8
sivasurya2347 <pnds2003@gmail.com>
2024-02-27 20:27:31 +0530

    Basic.js

--- basic.js
+++ basic.js
@@ -1,24 +1,16 @@
-{/* <div id='parent'>
-    <div id='child'>
-        <h1>I am h1</h1>
-        <h2>I am h2</h2>
-    </div>
-    <div id='child1'>
-        <h1>I am h1</h1>
-        <h2>I am h2</h2>
-    </div>
-</div> */}
+import React from 'react';
+import ReactDOM  from 'react-dom/client';
+// const parent=React.createElement('div',{id:'parent'},
+// [React.createElement('div',{id:'child'},
+// [React.createElement('h1',{},"I am h1"),React.createElement('h2',{},"I am h2")]),
+// React.createElement('div',{id:'child1'},
+// [React.createElement('h1',{},"I am h1"),React.createElement('h2',{},"I am h2")])
+// ]
+// )
 
-
-const parent=React.createElement('div',{id:'parent'},
-[React.createElement('div',{id:'child'},
-[React.createElement('h1',{},"I am h1"),React.createElement('h2',{},"I am h2")]),
-React.createElement('div',{id:'child1'},
-[React.createElement('h1',{},"I am h1"),React.createElement('h2',{},"I am h2")])
-]
-)
-
+// (JSX)=>Babel transpiles it to React.createElement=>Reactelement-js object=>html element
+const jsxheading =<h1 id="heading">React using JSX</h1>
 
 // const heading=React.createElement('h1',{id:'heading'},'Hello World By React')
 const root=ReactDOM.createRoot(document.getElementById('root'))
-root.render(parent)
+root.render(jsxheading)
