168e00fc155693208ff209383d93405e88cd88c5
sivasurya2347 <pnds2003@gmail.com>
2024-02-28 22:55:43 +0530

    RestaurantCard

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,20 @@
+import { CDN_URL } from "../utils/url"
+
+const RestaurantCard= (props) => {
+    const {RestData} = props
+    const {cloudinaryImageId , id , name ,costForTwo ,cuisines}=RestData?.info
+    console.log(props)
+   return(
+     <div className='res-card' style={{
+        backgroundColor:"orange"
+     }}>
+        <img alt='Barbeque Nation' className='res-logo' src={CDN_URL + cloudinaryImageId}></img>
+        <h4>{id}</h4>
+        <h4>{name}</h4>
+        <h4>Cost ForTwo{costForTwo}</h4>
+        <h4>{cuisines.join(', ')}</h4>
+     </div>
+   )
+}
+
+export default RestaurantCard;
