6c680be5dfb6cd4b95d2a1282c877784e690729f
sivasurya2347 <pnds2003@gmail.com>
2024-02-28 22:55:07 +0530

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import { LOGO_URL } from "../utils/url";
+
+const Header = () => {
+    return(
+    <div className='Header'>
+      <div className='Logo-container'>
+            <img src={LOGO_URL} className='logo'/>
+      </div>
+      <div className='nav-items'>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>
+    )
+}
+
+export default Header;
