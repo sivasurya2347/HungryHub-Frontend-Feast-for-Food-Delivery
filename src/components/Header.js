d63e8fc03157f6f403514bf83096b616519be861
sivasurya2347 <pnds2003@gmail.com>
2024-03-03 10:03:39 +0530

    Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import { LOGO_URL } from "../utils/url";
-import { useState } from "react";
+import { useState , useEffect } from "react";
+import { Link } from "react-router-dom";
 const Header = () => {
     const [Lbtn,setLbtn] = useState("Login");
+
+    useEffect(() => {
+        console.log("UseEffect called")
+    },[])
+    console.log("Header rendered")
     return(
     <div className='Header'>
       <div className='Logo-container'>
@@ -9,9 +15,15 @@ const Header = () => {
       </div>
       <div className='nav-items'>
             <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
+                <li>
+                <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About Us</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact Us</Link>
+                </li>
                 <li>Cart</li>
                 <button className="login-btn" onClick={()=>{
                  Lbtn==="Login"? setLbtn("Logout") : setLbtn("Login");
