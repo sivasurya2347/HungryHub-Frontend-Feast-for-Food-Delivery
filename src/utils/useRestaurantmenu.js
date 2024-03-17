6844f4558b2ffb4a91a7bdaabbba14e9c3b37f1d
sivasurya2347 <pnds2003@gmail.com>
2024-03-17 15:01:52 +0530

    useRestaurantmenu.js

---
+++ src/utils/useRestaurantmenu.js
@@ -0,0 +1,20 @@
+import { useState ,useEffect } from "react";
+import { Menu_API } from "../utils/url";
+
+const useRestaurantmenu = (restId) =>{
+    const [RestaurantData,setRestarantData]=useState(null)
+
+    useEffect(()=>{
+      fetchMenu();
+    },[])
+
+    const fetchMenu = async () =>{
+        const data = await fetch(Menu_API+restId);
+        const json = await data.json();
+        console.log(json)
+        setRestarantData(json?.data)
+    }
+    return RestaurantData;
+}
+
+export default useRestaurantmenu;
