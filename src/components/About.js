9299a370f230dbdb9825549aecaf5813912ccc53
sivasurya2347 <pnds2003@gmail.com>
2024-03-17 14:59:36 +0530

    About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,28 @@
-const About = () =>{
-    return(
-        <div>
-            <h1>About</h1>
-            <h2>This is About Us page</h2>
-        </div>
-    )
+import User from "./User";
+import Userclass from "./Userclass";
+import React from "react";
+
+class About extends React.Component{
+    constructor(){
+        super()
+        console.log("parent constructor")
+    }
+
+    componentDidMount(){
+       console.log("Parent Mount");
+    }
+    render(){
+
+        console.log("Parent Render")
+        return(
+            <div>
+                <h1>About</h1>
+                <h2>This is About Us page</h2>
+                <Userclass/>
+            </div>
+        )
+
+    }
 }
 
 export default About;
