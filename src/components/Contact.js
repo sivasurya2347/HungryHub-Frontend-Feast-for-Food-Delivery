ee42555ba152499eab26650a63959d41741d94b8
sivasurya2347 <pnds2003@gmail.com>
2024-05-12 15:04:51 +0530

    Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,12 @@
 const Contact = () =>{
     return(
         <div>
-            <h1>Contact Us</h1>
-            <h2>This is Contact Us page</h2>
+            <h1 className=" text-2xl">Contact Us</h1>
+            <input type="text"
+            className="border border-black m-5 p-3" placeholder="name"/>
+            <input type="text"
+            className="border border-black m-5 p-3" placeholder="message"/>
+            <button className="border border-black rounded-lg m-5 p-3 bg-slate-200">Submit</button>
         </div>
     )
 }
